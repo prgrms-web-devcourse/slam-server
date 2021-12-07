d40a48254f15fcd046ab9b2aa2a6577cff72fa81
SEO DONGSUNG <51287886+sds1vrk@users.noreply.github.com>
2021-12-07 18:50:12 +0900

    FEATURE/SLAM-45 court,reservation entity 추가 (#2)
    * FEATURE/SLAM-45 court,reservation entity 추가
    
    * 코드 리뷰 반영
    
    Co-authored-by: sds <sds1zzang@naver.com>

--- src/main/java/org/slams/server/court/entity/Texture.java
+++ src/main/java/org/slams/server/court/entity/Texture.java
@@ -1,7 +1,9 @@
 package org.slams.server.court.entity;
 
 /**
- * Created by yunyun on 2021/12/03.
+ * Created by dongsung on 2021/12/03.
  */
 public enum Texture {
+    RUBBER,URETHANE,ASPHALT,SOIL,CONCRETE,ETC
+
 }
