11a8aff99e09184ebd791bd6686ad82beae95d44
yekyeong <kwonliz6.6@gmail.com>
2021-12-07 15:12:59 +0900

    refactor : response 형식 수정

--- src/main/java/org/slams/server/common/api/CursorPageResponse.java
+++ src/main/java/org/slams/server/common/api/CursorPageResponse.java
@@ -1,13 +1,16 @@
 package org.slams.server.common.api;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class CursorPageResponse<T> {
 
-	private T data;
+	private T content;
 	private Long lastId;
 
 }
