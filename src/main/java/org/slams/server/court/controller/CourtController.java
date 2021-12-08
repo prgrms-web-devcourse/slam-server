76b5b787bac25c97dc3f2e79d65547745d44c264
sds <sds1vrk@gmail.com>
2021-12-08 17:25:52 +0900

    userTempRepository 추가

--- src/main/java/org/slams/server/court/controller/CourtController.java
+++ src/main/java/org/slams/server/court/controller/CourtController.java
@@ -2,9 +2,11 @@ package org.slams.server.court.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.slams.server.common.api.ApiResponse;
+import org.slams.server.court.dto.request.CourtInsertRequestDto;
 import org.slams.server.court.service.CourtService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -16,6 +18,23 @@ public class CourtController {
     public CourtController(CourtService courtService) {
         this.courtService=courtService;
     }
+
+
+    // 사용자에 의한 코트 추가
+    @PostMapping("/{id}/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<?> insert(@RequestBody CourtInsertRequestDto request, @PathVariable final Long id) {
+        return ApiResponse.ok(CourtService.insert(request, id));
+    }
+
+    // 전체 코트 조회
+//    @GetMapping
+//    public ApiResponse<Page<PostDetailResponse>> getAll(Pageable pageable) {
+//        return ApiResponse.ok(postService.findAll(pageable));
+//    }
+
+
+
 
 
 }
--- src/main/java/org/slams/server/court/service/CourtService.java
+++ src/main/java/org/slams/server/court/service/CourtService.java
@@ -1,7 +1,50 @@
 package org.slams.server.court.service;
 
+import org.slams.server.court.dto.request.CourtInsertRequestDto;
+import org.slams.server.court.entity.Court;
+import org.slams.server.court.repository.CourtRepository;
+import org.slams.server.court.repository.UserTempRepository;
+import org.slams.server.user.entity.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CourtService {
+
+    private final CourtRepository courtRepository;
+    private final UserTempRepository userTempRepository;
+
+
+
+    public CourtService(CourtRepository courtRepository, UserTempRepository userTempRepository) {
+        this.courtRepository=courtRepository;
+        this.userTempRepository=userTempRepository;
+    }
+
+
+    @Transactional
+    public void insert(CourtInsertRequestDto request, Long id) {
+        // user검색후 없으면 반환
+        User user = getUser(id);
+        Court court = request.insertRequestDtoToEntity(request, user);
+
+        post.addPost(user);
+
+        Post insertedPost = postRepository.save(post);
+        return new PostDetailResponse(insertedPost);
+    }
+
+
+    @Transactional
+    public User getUser(Long userId) {
+        return userTempRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_EXIST_MEMBER));
+    }
+
+
+
+
+
+
+
 }
