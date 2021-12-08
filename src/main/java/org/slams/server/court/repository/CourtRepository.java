7b84188c9ea4499b098e2ffd76546ad4dd64fffd
sds <sds1vrk@gmail.com>
2021-12-08 17:25:52 +0900

    userTempRepository 추가

--- src/main/java/org/slams/server/court/repository/CourtRepository.java
+++ src/main/java/org/slams/server/court/repository/CourtRepository.java
@@ -1,8 +1,7 @@
 package org.slams.server.court.repository;
 
 import org.slams.server.court.entity.Court;
-import org.slams.server.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserTempRepository extends JpaRepository<User, Long> {
+public interface CourtRepository extends JpaRepository<Court, Long> {
 }
