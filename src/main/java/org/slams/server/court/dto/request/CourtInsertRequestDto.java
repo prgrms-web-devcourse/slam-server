a408b521a8fd2c137ae13095c96e11c3a3c6dde4
sds <sds1vrk@gmail.com>
2021-12-08 17:38:45 +0900

    dto 작성

--- src/main/java/org/slams/server/court/dto/request/CourtInsertRequestDto.java
+++ src/main/java/org/slams/server/court/dto/request/CourtInsertRequestDto.java
@@ -1,4 +1,27 @@
 package org.slams.server.court.dto.request;
 
+import org.slams.server.court.entity.Court;
+
 public class CourtInsertRequestDto {
+
+    private String name;
+    private String email;
+    private String picture;
+    private Double temperature;
+    private String address;
+    private String phoneNumber;
+
+
+    // requestDto -> Entity
+    public Court insertRequestDtoToEntity(UserInsertRequestDto requestDto) {
+        return User.builder()
+                .name(requestDto.getName())
+                .email(requestDto.getEmail())
+                .picture(requestDto.getPicture())
+                .role(requestDto.getRole())
+                .temperature(requestDto.getTemperature())
+                .address(requestDto.getAddress())
+                .phoneNumber(requestDto.getPhoneNumber())
+                .build();
+    }
 }
