ce98bb05c49332f59bad9fbc124a823ce73167a4
yunyun <locodingve@gmail.com>
2021-12-10 09:47:16 +0900

    feat: alarm dto 추가

---
+++ src/main/java/org/slams/server/alarm/dto/AlarmRequest.java
@@ -0,0 +1,38 @@
+package org.slams.server.alarm.dto;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+/**
+ * Created by yunyun on 2021/12/08.
+ */
+
+@Getter
+public class AlarmRequest {
+    private final Long courtId;
+    private final String courtName;
+    private final String startDate;
+    private final int startTime;
+    private final long userId;
+    private final Long reservationId;
+
+    public AlarmRequest(Long courtId, String courtName, String startDate, int startTime, long userId, Long reservationId){
+        checkArgument(courtId != null, "courtId는 null을 허용하지 않습니다.");
+        checkArgument(isNotEmpty(courtName), "courtName는 빈값을 허용하지 않습니다.");
+        checkArgument(isNotEmpty(startDate), "startDate는 빈값을 허용하지 않습니다.");
+
+        checkArgument(0 <= startTime && startTime < 25, "startTime은 0이상 24이하여야 합나다.");
+        checkArgument(userId > 0, "userId는 0미만의 값을 허용하지 않습니다.");
+        checkArgument(reservationId != null, "reservationId는 null을 허용하지 않습니다.");
+
+        this.courtId = courtId;
+        this.courtName = courtName;
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.userId = userId;
+        this.reservationId = reservationId;
+    }
+}
---
+++ src/main/java/org/slams/server/alarm/dto/AlarmResponse.java
@@ -0,0 +1,7 @@
+package org.slams.server.alarm.dto;
+
+/**
+ * Created by yunyun on 2021/12/08.
+ */
+public class AlarmResponse {
+}
