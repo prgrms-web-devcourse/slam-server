4666d197085ccbe6ec0643343f8ff9ee1d87e6f0
sds <sds1vrk@gmail.com>
2021-12-10 18:15:24 +0900

    REFACTOR Api Reponse 수정

--- src/main/java/org/slams/server/common/api/ApiResponse.java
+++ src/main/java/org/slams/server/common/api/ApiResponse.java
@@ -10,25 +10,26 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ApiResponse<T> {
 
-	private Boolean success;
-	private T data;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-ddTHH:mm:ss", timezone = "Asia/Seoul")
-	private LocalDateTime serverDateTime;
 
-	public ApiResponse(Boolean success, T data) {
-		this.success = success;
+	private T data;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime serverDateTime;
+	private Boolean success;
+
+	public ApiResponse(T data, Boolean success) {
 		this.data = data;
 		this.serverDateTime = LocalDateTime.now();
+		this.success=success;
 	}
 
 	public static <T> ApiResponse<T> ok(T data) {
-		String message = "save";
-
-		return new ApiResponse<>(true, data);
+		return new ApiResponse<>(data,true);
 	}
 
 	public static <T> ApiResponse<T> fail(T data) {
-		return new ApiResponse<>(false, data);
+		return new ApiResponse<>(data,false);
 	}
+
+
 
 }
