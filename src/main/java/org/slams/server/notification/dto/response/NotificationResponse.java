5261eb60d682cefe48502e36ad0b70a70239ba0f
yunyun <locodingve@gmail.com>
2021-12-15 13:49:55 +0900

    fix : dto created, updated 추가

---
+++ src/main/java/org/slams/server/notification/dto/response/NotificationResponse.java
@@ -0,0 +1,67 @@
+package org.slams.server.notification.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.slams.server.notification.entity.NotificationType;
+
+import java.time.LocalDateTime;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * Created by yunyun on 2021/12/08.
+ */
+
+@Getter
+public class NotificationResponse {
+    private final NotificationType type;
+    private final FollowerInfo followerInfo;
+    private final LoudspeakerInfo loudspeakerInfo;
+    private final boolean isRead;
+    private final boolean isClicked;
+    private final LocalDateTime created;
+    private final LocalDateTime updated;
+
+
+    private NotificationResponse(
+            NotificationType type,
+            FollowerInfo followerInfo,
+            LoudspeakerInfo loudspeakerInfo,
+            boolean isRead,
+            boolean isClicked,
+            LocalDateTime created,
+            LocalDateTime updated
+    ){
+        this.type = type;
+        this.followerInfo = followerInfo;
+        this.loudspeakerInfo = loudspeakerInfo;
+        this.isRead = isRead;
+        this.isClicked = isClicked;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public static NotificationResponse createForFollowNotification(
+            NotificationType type,
+            FollowerInfo followerInfo,
+            boolean isRead,
+            boolean isClicked,
+            LocalDateTime created,
+            LocalDateTime updated
+    ){
+        return new NotificationResponse(type, followerInfo, null, isRead, isClicked, created, updated);
+    }
+
+    public static NotificationResponse createForLoudspeakerNotification(
+            NotificationType type,
+            LoudspeakerInfo loudspeakerInfo,
+            boolean isRead,
+            boolean isClicked,
+            LocalDateTime created,
+            LocalDateTime updated
+    ){
+        return new NotificationResponse(type, null, loudspeakerInfo, isRead, isClicked, created, updated);
+    }
+
+
+}
