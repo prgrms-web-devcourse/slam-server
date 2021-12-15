24a4faea3857f2ff68898386c3ae31a86fc09c82
yunyun <locodingve@gmail.com>
2021-12-15 14:18:09 +0900

    feat : websocket 기능 추가

---
+++ src/main/java/org/slams/server/notification/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package org.slams.server.notification.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Created by yunyun on 2021/12/15.
+ */
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/api/v1/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS()
+                .setStreamBytesLimit(512 * 1024)
+                .setHttpMessageCacheSize(1000)
+                .setDisconnectDelay(30 * 1000);;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //registry.setApplicationDestinationPrefixes("/");
+        registry.enableSimpleBroker("/topic");
+    }
+}
