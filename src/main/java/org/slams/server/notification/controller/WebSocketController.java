505e54a380391f7df13059b9ac1daaff2736278f
yunyun <locodingve@gmail.com>
2021-12-15 23:36:58 +0900

    feat : 헤더값 추출 완료

--- src/main/java/org/slams/server/notification/controller/WebSocketController.java
+++ src/main/java/org/slams/server/notification/controller/WebSocketController.java
@@ -1,5 +1,8 @@
 package org.slams.server.notification.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slams.server.notification.dto.SocketRequest;
 import org.slams.server.notification.dto.SocketResponse;
 import org.slams.server.notification.dto.UserRequest;
@@ -7,14 +10,19 @@ import org.slams.server.user.exception.InvalidTokenException;
 import org.slams.server.user.oauth.jwt.Jwt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yunyun on 2021/12/15.
@@ -32,31 +40,6 @@ public class WebSocketController {
         this.websoket = websoket;
         this.jwt = jwt;
     }
-    //@MessageMapping("/chat")
-//    @SendTo("/topic/chat")
-//    public MessageResponse testChat(@Payload MessageRequest message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-//        logger.info(message.toString());
-//        String token = (String) headerAccessor.getHeader("token");
-//        /** token parsing **/
-//        // userId 추출
-//        Long testUserId = 1L;
-//        // token 유효성 검사
-//
-//        return new MessageResponse(testUserId, message.getMessage(), MessageType.LOUDSPEAKER);
-//    }
-
-//    @MessageMapping("/follow")
-//    @SendTo("/topic/follow")
-//    public MessageResponse testFollow(@Payload MessageRequest message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-//        logger.info(message.toString());
-//        String token = (String) headerAccessor.getHeader("token");
-//        /** token parsing **/
-//        // userId 추출
-//        Long testUserId = 1L;
-//        // token 유효성 검사
-//
-//        return new MessageResponse(testUserId, message.getMessage(), MessageType.FOLLOW);
-//    }
 
     @MessageMapping("/teston")
     public void testNone() throws Exception {
@@ -86,28 +69,12 @@ public class WebSocketController {
     }
 
     @MessageMapping("/object")
-    public void objectTest(UserRequest message, ServletServerHttpRequest request) throws Exception {
-        logger.info("들어옴");
-        logger.info(message.toString());
+    public void objectTest(UserRequest message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
 
-        ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
-        HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
+        var nativeHeaders =  (Map) headerAccessor.getMessageHeaders().get("nativeHeaders");
+        String token = nativeHeaders.get("token").toString().replace("[", "").replace("]","");
+        Long testUserId = jwt.verify(token).getUserId();
 
-
-        String token = httpServletRequest.getParameter("token");
-        //String token = (String) headerAccessor.getHeader("token");
-        /** token parsing **/
-
-        logger.info( token);
-
-        // userId 추출
-        String[] tokenString = token.split(" ");
-        if (!tokenString[0].equals("Bearer")) {
-            throw new InvalidTokenException("토큰 정보가 올바르지 않습니다.");
-        }
-
-
-        Long testUserId = jwt.verify(tokenString[1]).getUserId();
         // token 유효성 검사
         UserRequest userRequest = new UserRequest();
         userRequest.setUserId(message.getUserId());
