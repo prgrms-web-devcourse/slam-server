f57233d0038e361bb5ef6e13bb824912ed1d18c8
yekyeong <kwonliz6.6@gmail.com>
2021-12-16 18:02:43 +0900

    feat : 팔로워 목록 무한 스크롤로 조회 api 구현

--- src/main/java/org/slams/server/follow/repository/FollowRepository.java
+++ src/main/java/org/slams/server/follow/repository/FollowRepository.java
@@ -2,7 +2,6 @@ package org.slams.server.follow.repository;
 
 import org.slams.server.follow.entity.Follow;
 import org.slams.server.user.entity.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
