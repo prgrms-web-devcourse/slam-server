aa5a7fe42fc8ff542e8907969ae97d0c4f81da4b
KwonYeKyeong <65434196+KwonYeKyeong@users.noreply.github.com>
2021-12-17 04:59:14 +0900

    Feature/slam 8 - 팔로잉 목록 무한스크롤로 조회 api (#28)
    * feat : 팔로잉 목록 무한 스크롤로 조회 api 구현
    
    * test : 팔로잉 목록 무한 스크롤로 조회 api ControllerTest 작성

--- src/main/java/org/slams/server/follow/controller/FollowController.java
+++ src/main/java/org/slams/server/follow/controller/FollowController.java
@@ -3,7 +3,8 @@ package org.slams.server.follow.controller;
 import lombok.RequiredArgsConstructor;
 import org.slams.server.common.api.CursorPageRequest;
 import org.slams.server.common.api.CursorPageResponse;
-import org.slams.server.follow.dto.FollowResponse;
+import org.slams.server.follow.dto.FollowerResponse;
+import org.slams.server.follow.dto.FollowingResponse;
 import org.slams.server.follow.service.FollowService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,19 @@ public class FollowController {
 	private final FollowService followService;
 
 	@GetMapping("/{userId}/followers")
-	public ResponseEntity<CursorPageResponse<List<FollowResponse>>> followerPage(
+	public ResponseEntity<CursorPageResponse<List<FollowerResponse>>> followerPage(
 		@PathVariable Long userId, CursorPageRequest cursorPageRequest) {
-		CursorPageResponse<List<FollowResponse>> followerResponse = followService.followerPage(userId, cursorPageRequest);
+		CursorPageResponse<List<FollowerResponse>> followerResponse = followService.followerPage(userId, cursorPageRequest);
 
 		return ResponseEntity.ok(followerResponse);
+	}
+
+	@GetMapping("/{userId}/followings")
+	public ResponseEntity<CursorPageResponse<List<FollowingResponse>>> followingPage(
+		@PathVariable Long userId, CursorPageRequest cursorPageRequest) {
+		CursorPageResponse<List<FollowingResponse>> followingResponse = followService.followingPage(userId, cursorPageRequest);
+
+		return ResponseEntity.ok(followingResponse);
 	}
 
 }
--- src/main/java/org/slams/server/follow/dto/FollowingResponse.java
+++ src/main/java/org/slams/server/follow/dto/FollowingResponse.java
@@ -9,27 +9,27 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class FollowResponse {
+public class FollowingResponse {
 
 	private Long followId;
-	private Long creatorId;
+	private Long receiverId;
 	private String nickname;
 	private String profileImage;
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
 
-	private FollowResponse(Long followId, Long creatorId, String nickname, String profileImage,
-						   LocalDateTime createdAt, LocalDateTime updatedAt) {
+	private FollowingResponse(Long followId, Long receiverId, String nickname, String profileImage,
+							  LocalDateTime createdAt, LocalDateTime updatedAt) {
 		this.followId = followId;
-		this.creatorId = creatorId;
+		this.receiverId = receiverId;
 		this.nickname = nickname;
 		this.profileImage = profileImage;
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
 
-	public static FollowResponse toResponse(Long followId, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
-		return new FollowResponse(followId, user.getId(), user.getNickname(), user.getProfileImage(),
+	public static FollowingResponse toResponse(Long followId, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		return new FollowingResponse(followId, user.getId(), user.getNickname(), user.getProfileImage(),
 			createdAt, updatedAt);
 	}
 
