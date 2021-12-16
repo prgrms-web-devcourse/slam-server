0ff10428c03f4f5d571a3ba7e3def26074960045
yunyun <locodingve@gmail.com>
2021-12-16 22:03:43 +0900

    feat: convertor 추가

---
+++ src/main/java/org/slams/server/chat/convertor/ChatContentConvertor.java
@@ -0,0 +1,23 @@
+package org.slams.server.chat.convertor;
+
+import org.slams.server.chat.dto.ChatContentsResponse;
+import org.slams.server.chat.entity.ChatContents;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by yunyun on 2021/12/16.
+ */
+
+@Component
+public class ChatContentConvertor {
+
+    public List<ChatContentsResponse> toDtoList(List<ChatContents> chatContentsList){
+
+    }
+
+    public ChatContentsResponse toDto(ChatContents chatContents){
+
+    }
+}
---
+++ src/main/java/org/slams/server/chat/convertor/ChatRoomMappingConvertor.java
@@ -0,0 +1,22 @@
+package org.slams.server.chat.convertor;
+
+import org.slams.server.chat.dto.ChatRoomResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by yunyun on 2021/12/16.
+ */
+
+@Component
+public class ChatRoomMappingConvertor {
+
+    public List<ChatRoomResponse> toDtoList(){
+
+    }
+
+    public ChatRoomResponse toDto(){
+
+    }
+}
