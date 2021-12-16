886d97e56c5a0c300733db45b82426674e034bed
yunyun <locodingve@gmail.com>
2021-12-16 22:04:35 +0900

    feat: repository 추가

--- src/main/java/org/slams/server/chat/repository/ChatContentsRepository.java
+++ src/main/java/org/slams/server/chat/repository/ChatContentsRepository.java
@@ -1,10 +1,28 @@
 package org.slams.server.chat.repository;
 
 import org.slams.server.chat.entity.ChatContents;
+import org.slams.server.chat.entity.UserChatroomMapping;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by yunyun on 2021/12/16.
  */
 public interface ChatContentsRepository extends JpaRepository<ChatContents, Long> {
+    @Query("SELECT c FROM ChatContents c WHERE c.courtId = :courtId AND c.id >= :lastId ORDER BY c.createdAt ASC")
+    List<ChatContents> findAllByUserIdMoreThenLastIdByCreated(
+            @Param("userId") Long userId,
+            @Param("lastId") Long lastId,
+            Pageable pageable
+    );
+
+    @Query("SELECT c FROM ChatContents c WHERE c.courtId = :courtId ORDER BY c.createdAt ASC")
+    List<ChatContents> findAllByUserIdByCreated(
+            @Param("userId") Long userId,
+            Pageable pageable
+    );
 }
--- src/main/java/org/slams/server/chat/repository/UserChatRoomMappingRepository.java
+++ src/main/java/org/slams/server/chat/repository/UserChatRoomMappingRepository.java
@@ -1,10 +1,28 @@
 package org.slams.server.chat.repository;
 
 import org.slams.server.chat.entity.UserChatroomMapping;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by yunyun on 2021/12/16.
  */
 public interface UserChatRoomMappingRepository extends JpaRepository<UserChatroomMapping, Long> {
+
+    @Query("SELECT u FROM UserChatroomMapping u WHERE u.user.id = :userId AND u.id >= :lastId ORDER BY u.createdAt ASC")
+    List<UserChatroomMapping> findAllByUserIdMoreThenLastIdByCreated(
+            @Param("userId") Long userId,
+            @Param("lastId") Long lastId,
+            Pageable pageable
+    );
+
+    @Query("SELECT u FROM UserChatroomMapping u WHERE u.user.id = :userId ORDER BY u.createdAt ASC")
+    List<UserChatroomMapping> findAllByUserIdByCreated(
+            @Param("userId") Long userId,
+            Pageable pageable
+    );
 }
