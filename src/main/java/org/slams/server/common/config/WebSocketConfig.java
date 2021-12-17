18b10a66e3d99eb564d15072445fd52b22e64616
yunyun <locodingve@gmail.com>
2021-12-17 16:56:01 +0900

    config: websocket uri 변경

--- src/main/java/org/slams/server/common/config/WebSocketConfig.java
+++ src/main/java/org/slams/server/common/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/v1/ws")
+        registry.addEndpoint("/ws/v1")
                 .setAllowedOriginPatterns("*")
                 .withSockJS()
                 .setStreamBytesLimit(512 * 1024)
