9681896b0c7051364e649ceddbc9f0e466dad064
yekyeong <kwonliz6.6@gmail.com>
2021-12-18 05:56:30 +0900

    feat : 사용자가 추가한 농구장 조회 api 구현

--- src/main/java/org/slams/server/court/service/NewCourtService.java
+++ src/main/java/org/slams/server/court/service/NewCourtService.java
@@ -1,19 +1,26 @@
 package org.slams.server.court.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slams.server.common.api.CursorPageRequest;
+import org.slams.server.common.api.CursorPageResponse;
 import org.slams.server.court.dto.request.CourtInsertRequestDto;
 import org.slams.server.court.dto.response.CourtInsertResponseDto;
 import org.slams.server.court.dto.response.NewCourtResponse;
 import org.slams.server.court.entity.NewCourt;
+import org.slams.server.court.entity.Status;
+import org.slams.server.court.exception.InvalidStatusException;
 import org.slams.server.court.exception.NewCourtNotFoundException;
 import org.slams.server.court.repository.NewCourtRepository;
 import org.slams.server.user.entity.User;
 import org.slams.server.user.exception.UserNotFoundException;
 import org.slams.server.user.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -22,6 +29,37 @@ public class NewCourtService {
 
 	private final NewCourtRepository newCourtRepository;
 	private final UserRepository userRepository;
+
+	public CursorPageResponse<List<NewCourtResponse>> getNewCourtsInStatus(String status, CursorPageRequest cursorPageRequest) {
+		PageRequest pageable = PageRequest.of(0, cursorPageRequest.getSize());
+
+		List<NewCourt> newCourts;
+		switch (status) {
+			case "READY":
+				newCourts = cursorPageRequest.getIsFirst() ?
+					newCourtRepository.findByStatusOrderByIdDesc(List.of(Status.READY), pageable) :
+					newCourtRepository.findByStatusLessThanIdOrderByIdDesc(List.of(Status.READY), cursorPageRequest.getLastId(), pageable);
+				break;
+			case "DONE":
+				newCourts = cursorPageRequest.getIsFirst() ?
+					newCourtRepository.findByStatusOrderByIdDesc(List.of(Status.ACCEPT, Status.DENY), pageable) :
+					newCourtRepository.findByStatusLessThanIdOrderByIdDesc(List.of(Status.ACCEPT, Status.DENY), cursorPageRequest.getLastId(), pageable);
+				break;
+			default:
+				throw new InvalidStatusException(MessageFormat.format("잘못된 상태값입니다. status : {0}", status));
+		}
+
+		List<NewCourtResponse> newCourtList = new ArrayList<>();
+		for (NewCourt newCourt : newCourts) {
+			newCourtList.add(
+				NewCourtResponse.toResponse(newCourt)
+			);
+		}
+
+		Long lastId = newCourtList.size() < cursorPageRequest.getSize() ? null : newCourts.get(newCourts.size() - 1).getId();
+
+		return new CursorPageResponse<>(newCourtList, lastId);
+	}
 
 	@Transactional
 	public CourtInsertResponseDto insert(CourtInsertRequestDto request, Long id) {
@@ -41,7 +79,7 @@ public class NewCourtService {
 	}
 
 	@Transactional
-	public NewCourtResponse acceptNewCourt(Long newCourtId){
+	public NewCourtResponse acceptNewCourt(Long newCourtId) {
 		NewCourt newCourt = newCourtRepository.findById(newCourtId)
 			.orElseThrow(() -> new NewCourtNotFoundException(
 				MessageFormat.format("사용자가 추가한 농구장을 찾을 수 없습니다. id : {0}", newCourtId)
