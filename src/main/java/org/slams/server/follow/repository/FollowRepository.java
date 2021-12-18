f81e477fa52d2a0044d0de4b93cf9ab18cdd8456
KwonYeKyeong <65434196+KwonYeKyeong@users.noreply.github.com>
2021-12-19 04:32:15 +0900

    Fix/followDeleteError - @Modifying을 붙여서 에러 해결 (#54)

--- src/main/java/org/slams/server/follow/repository/FollowRepository.java
+++ src/main/java/org/slams/server/follow/repository/FollowRepository.java
@@ -4,8 +4,10 @@ import org.slams.server.follow.entity.Follow;
 import org.slams.server.user.entity.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 
@@ -23,7 +25,7 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 	// 해당 유저의 팔로워 목록(무한 스크롤)
 	@Query("SELECT f FROM Follow f WHERE f.following.id = :followingId and f.id < :lastId order by f.id desc")
 	List<Follow> findByFollowingIdAndIdLessThanOrderByIdDesc(
-		@Param("followingId") Long followingId,@Param("lastId") Long lastId, Pageable pageable);
+		@Param("followingId") Long followingId, @Param("lastId") Long lastId, Pageable pageable);
 
 	// 해당 유저의 팔로잉 목록(무한 스크롤 - 최초)
 	@Query("SELECT f FROM Follow f WHERE f.follower.id = :followerId order by f.id desc")
@@ -31,12 +33,14 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 	// 해당 유저의 팔로잉 목록(무한 스크롤)
 	@Query("SELECT f FROM Follow f WHERE f.follower.id = :followerId and f.id < :lastId order by f.id desc")
 	List<Follow> findByFollowerIdAndIdLessThanOrderByIdDesc(
-		@Param("followerId") Long followerId,@Param("lastId") Long lastId, Pageable pageable);
+		@Param("followerId") Long followerId, @Param("lastId") Long lastId, Pageable pageable);
 
 	// 팔로우 관계를 맺고있는지 확인
 	Boolean existsByFollowerAndFollowing(User follower, User following);
 
 	// 팔로우 관계 삭제
-	void deleteByFollowerAndFollowing(User follower, User following);
+	@Modifying
+	@Query("DELETE FROM Follow f WHERE f.follower.id = :followerId and f.following.id = :followingId")
+	void deleteByFollowerAndFollowing(@Param("followerId") Long followerId, @Param("followingId") Long followingId);
 
 }
