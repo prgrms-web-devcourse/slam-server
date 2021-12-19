870cc0f4d598a24a3bca10e709e626db52fc707d
sds <sds1vrk@gmail.com>
2021-12-19 19:48:35 +0900

    fix : boundary로 유저 조회 (group by로 중복 제거)

--- src/main/java/org/slams/server/reservation/repository/ReservationRepository.java
+++ src/main/java/org/slams/server/reservation/repository/ReservationRepository.java
@@ -25,6 +25,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("endLocalDateTime") LocalDateTime endLocalDateTime
            );
 
+//    @Query("SELECT r FROM Reservation r WHERE r.user.id=:userId AND r.startTime>:localDateTime")
+//    List<Reservation> findByUserByNow(
+//            @Param("userId") Long userId,
+//            @Param ("localDateTime") LocalDateTime localDateTime);
+    // 수정
     @Query("SELECT r FROM Reservation r WHERE r.user.id=:userId AND r.startTime>:localDateTime")
     List<Reservation> findByUserByNow(
             @Param("userId") Long userId,
@@ -37,7 +42,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
 
 //    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.court.id=:courtId")
-    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.court.id=:courtId AND ((r.startTime BETWEEN :startLocalDateTime AND :endLocalDateTime) OR (r.endTime BETWEEN :startLocalDateTime AND :endLocalDateTime))")
+    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.court.id=:courtId AND ((r.startTime BETWEEN :startLocalDateTime AND :endLocalDateTime) OR (r.endTime BETWEEN :startLocalDateTime AND :endLocalDateTime))GROUP BY r.user.id")
     Long findByDate(
             @Param("startLocalDateTime") LocalDateTime startLocalDateTime,
             @Param("endLocalDateTime") LocalDateTime endLocalDateTime,
@@ -53,6 +58,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("eTime") LocalDateTime eTime);
 
     // 유저의 지난 지난 예약 조회 (무한 스크롤 - 최초)
+//    @Query("SELECT r FROM Reservation r WHERE r.user.id=:userId AND r.endTime<:localDateTime ORDER BY r.id desc")
+//    List<Reservation> findByUserByExpiredOrderByDesc(
+//            @Param("userId") Long userId,
+//            @Param ("localDateTime") LocalDateTime localDateTime, Pageable pageable);
+
+    // 수정
     @Query("SELECT r FROM Reservation r WHERE r.user.id=:userId AND r.endTime<:localDateTime ORDER BY r.id desc")
     List<Reservation> findByUserByExpiredOrderByDesc(
             @Param("userId") Long userId,
