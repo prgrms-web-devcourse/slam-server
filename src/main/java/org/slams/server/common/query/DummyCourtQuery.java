acc6dd77dd3d437173f825522ea72b469b507b8a
sds <sds1vrk@gmail.com>
2021-12-20 09:30:25 +0900

    fix : 더미 데이터 위도, 경도 위치 수정

--- src/main/java/org/slams/server/common/query/DummyCourtQuery.java
+++ src/main/java/org/slams/server/common/query/DummyCourtQuery.java
@@ -40,9 +40,9 @@ public class DummyCourtQuery {
     @Transactional
     public void insert() {
         final List<Court> details= Arrays.asList(
-                new Court("잠실한강공원농구장",127.082348760182,37.5177740347208,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court1.jpg",2,Texture.ASPHALT),
-                new Court("뚝섬농구장",127.077891070963,37.5277871954486,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court2.jpg",3,Texture.RUBBER),
-                new Court("광진구 헤이 집 농구장",127.074164786264,37.6012425773731,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court3.jpg",4,Texture.CONCRETE),
+                new Court("잠실한강공원농구장",37.5177740347208,127.082348760182,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court1.jpg",2,Texture.ASPHALT),
+                new Court("뚝섬농구장",37.5277871954486,127.077891070963,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court2.jpg",3,Texture.RUBBER),
+                new Court("광진구 헤이 집 농구장",37.6012425773731,127.074164786264,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court3.jpg",4,Texture.CONCRETE),
                 new Court("헤이집1",55.00,201.00,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court1.jpg",2,Texture.ASPHALT),
                 new Court("헤이집2",45.00,299.00,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court2.jpg",3,Texture.RUBBER),
                 new Court("헤이집3",99.00,345.00,"https://team14-slam.s3.ap-northeast-2.amazonaws.com/court_dummy/court3.jpg",4,Texture.CONCRETE)
