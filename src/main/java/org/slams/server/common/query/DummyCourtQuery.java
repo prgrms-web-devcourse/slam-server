37ffc146c4d4add34add48a901bfb2dc9c2bfcfb
yunyun <locodingve@gmail.com>
2021-12-20 12:40:06 +0900

    fix : 더미 데이터 엑셀파일로 읽고 저장 이슈 해결 시도 2

--- src/main/java/org/slams/server/common/query/DummyCourtQuery.java
+++ src/main/java/org/slams/server/common/query/DummyCourtQuery.java
@@ -1,6 +1,5 @@
 package org.slams.server.common.query;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -15,10 +14,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ResourceUtils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -71,14 +69,15 @@ public class DummyCourtQuery {
 
     @Transactional
     public void insertExcel() throws IOException, InvalidFormatException {
-        URL res = getClass().getClassLoader().getResource("dummyCourt.xlsx");
+//        URL res = getClass().getClassLoader().getResource(excelFileName);
 
-        logger.info(String.valueOf(res));
         List<Court> dataList = new ArrayList<>();
 
-        String dumpDataLocation = ResourceUtils.getFile(resourceLocation + excelFileName).getAbsolutePath();
+        try (
+//                FileInputStream file = new FileInputStream(dumpDataLocation);
+                InputStream file = new ClassPathResource(excelFileName).getInputStream();
 
-        try (FileInputStream file = new FileInputStream(dumpDataLocation)) {
+        ) {
 
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             Sheet worksheet = workbook.getSheetAt(0);
@@ -104,6 +103,7 @@ public class DummyCourtQuery {
             dummyCourtRepository.saveAll(dataList);
 
         } catch (Exception e) {
+            logger.info(e.getMessage());
             logger.error("insert dumy data ERROR");
             insert();
         }
