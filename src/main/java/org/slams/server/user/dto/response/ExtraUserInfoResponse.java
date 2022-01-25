3a99f2dd9036f40f54014e145b0e5e0abe8a9df9
yekyeong <kwonliz6.6@gmail.com>
2022-01-25 23:11:43 +0900

    refactor : DefaultUserDto 적용하여 response 수정

--- src/main/java/org/slams/server/user/dto/response/ExtraUserInfoResponse.java
+++ src/main/java/org/slams/server/user/dto/response/ExtraUserInfoResponse.java
@@ -17,34 +17,14 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExtraUserInfoResponse {
 
-	private Long userId;
-	private String email;
-	private String nickname;
-	private String profileImage;
-	private String description;
-	private Role role;
-	private Proficiency proficiency;
-	private List<Position> positions;
-	private LocalDateTime createdAt;
-	private LocalDateTime updatedAt;
+	private DefaultUserDto user;
 
-	private ExtraUserInfoResponse(Long userId, String email, String nickname, String profileImage, String description, Role role,
-								  Proficiency proficiency, List<Position> positions, LocalDateTime createdAt, LocalDateTime updatedAt) {
-		this.userId = userId;
-		this.email = email;
-		this.nickname = nickname;
-		this.profileImage = profileImage;
-		this.description = description;
-		this.role = role;
-		this.proficiency = proficiency;
-		this.positions = positions;
-		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
+	public ExtraUserInfoResponse(DefaultUserDto user) {
+		this.user = user;
 	}
 
 	public static ExtraUserInfoResponse toResponse(User user) {
-		return new ExtraUserInfoResponse(user.getId(), user.getEmail(), user.getNickname(), user.getProfileImage(),
-			user.getDescription(), user.getRole(), user.getProficiency(), user.getPositions(), user.getCreatedAt(), user.getUpdateAt());
+		return new ExtraUserInfoResponse(DefaultUserDto.toDto(user));
 	}
 
 }
